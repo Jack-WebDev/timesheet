58d8955e4b1fd5e74d9c6e4e3f83c6fab3c11f01
Jack WebDev <jackbuda31@gmail.com>
2024-04-13 13:54:04 +0200

    data table done

--- frontend/src/components/Columns.tsx
+++ frontend/src/components/Columns.tsx
@@ -1,25 +1,47 @@
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
-
+import { ColumnDef } from "@tanstack/react-table";
+import {FaEdit, FaTrash} from "react-icons/fa"
 
 export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-}
+	id: string;
+	employee: string,
+	department: string,
+	email: string;
+	status: string
+};
 
 export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-  },
-]
+	{
+		accessorKey: "employee",
+		header: "Employee Name",
+		
+	},
+	{
+		accessorKey: "email",
+		header: "Email"
+	},
+	{
+		accessorKey: "department",
+		header: "Department Name",
+	},
+	{
+		accessorKey: "status",
+		header: "Status",
+	},
+	{
+		id: "actions",
+		accessorKey: "actions",
+		header: "Actions",
+		cell: () => {
+
+			return (
+				<div className="flex items-center gap-x-4">
+					<p><FaEdit/> Edit</p>
+					<p><FaTrash/> Delete</p>
+				</div>
+			);
+		},
+	},
+
+];
--- frontend/src/components/DataTable.tsx
+++ frontend/src/components/DataTable.tsx
@@ -1,80 +1,134 @@
-"use client"
+"use client";
 
 import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table"
+	ColumnDef,
+	SortingState,
+	flexRender,
+	getSortedRowModel,
+	getCoreRowModel,
+	getPaginationRowModel,
+	useReactTable,
+	ColumnFiltersState,
+	getFilteredRowModel,
+  VisibilityState,
+} from "@tanstack/react-table";
+
+import { Input } from "@/components/ui/input";
+
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import React from "react";
+import DialogDemo from "./AddButton";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+	columns: ColumnDef<TData, TValue>[];
+	data: TData[];
 }
 
 export function DataTable<TData, TValue>({
-  columns,
-  data,
+	columns,
+	data,
 }: DataTableProps<TData, TValue>) {
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  })
+	const [sorting, setSorting] = React.useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+		[]
+	);
+  const [columnVisibility, setColumnVisibility] =
+  React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
 
-  return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  )
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		onSortingChange: setSorting,
+		getSortedRowModel: getSortedRowModel(),
+		onColumnFiltersChange: setColumnFilters,
+		getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+	});
+
+	return (
+		<div className="w-1/2 m-auto">
+			<div className="flex items-center py-4 justify-between">
+				<Input
+					placeholder="Filter emails..."
+					value={(table.getColumn("employee")?.getFilterValue() as string) ?? ""}
+					onChange={(event) =>
+						table.getColumn("employee")?.setFilterValue(event.target.value)
+					}
+					className="w-1/2"
+				/>
+				<DialogDemo/>
+			</div>
+			<div className="rounded-md border">
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext()
+												  )}
+										</TableHead>
+									);
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && "selected"}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center"
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+
+			</div>
+		</div>
+	);
 }
